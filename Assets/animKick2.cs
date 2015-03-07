3456451a7d3923dc5b2ed3421151f2b1ad9683a7
Symon799 <simon.rigard@epita.fr>
2015-03-07 16:30:25 +0100

    Added Oculus support for s1 in VR2 changed terrain to flat minor changes of the zombies and stuff...

---
+++ Assets/animKick2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animKick2 : MonoBehaviour {
+	
+	public Transform center;
+	public float hitdistance;
+
+	void Start ()
+	{
+		animation["Kick"].wrapMode = WrapMode.Once;
+		animation["Kick"].layer = 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (Input.GetButtonDown ("Kick")&& !animation.IsPlaying("Kick"))
+		{
+			animation.CrossFade("Kick");
+			RaycastHit hit;
+			Ray ray = new Ray(center.position, center.transform.forward);
+			if(Physics.Raycast(ray, out hit))
+			{
+				if (hit.collider.tag == "Zombie")
+				{
+					hitdistance = hit.distance;
+					//Debug.DrawLine (center.position, hit.point, Color.cyan);
+					if(hit.distance < 2.5f)
+					{
+						Debug.Log("Working bitch");
+						GameObject go = hit.collider.gameObject;
+						mAI other = (mAI)go.GetComponent (typeof(mAI));
+						other.Kick();
+					}
+				}
+			}
+		}
+	}
+}
