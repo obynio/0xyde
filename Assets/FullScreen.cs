5b442e770bba8a0b326ce68849c0e306c241771e
Symon799 <simon.rigard@epita.fr>
2015-03-08 18:57:33 +0100

    Minor fixes

--- Assets/FullScreen.cs
+++ Assets/FullScreen.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class FullScreen : MonoBehaviour {
 
-	public Texture2D cursor;
-	private Vector2 mouse;
 	private int w = 32;
 	private int h = 32;
 	
@@ -12,17 +10,7 @@ public class FullScreen : MonoBehaviour {
 	{
 		Screen.showCursor = false;
 	}
-	
-	void Update()
-	{
-		mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
-	}
-	
-	void OnGUI()
-	{
-		GUI.DrawTexture(new Rect(mouse.x - (w / 2), mouse.y - (h / 2), w, h), cursor);
-	}
-	
+
 	void OnMouseEnter()
 	{
 		transform.localScale *= 1.1f;
--- Assets/Sound.cs
+++ Assets/Sound.cs
@@ -3,26 +3,13 @@ using System.Collections;
 
 public class Sound : MonoBehaviour {
 
-	public Texture2D cursor;
-	private Vector2 mouse;
 	private int w = 32;
 	private int h = 32;
 	bool mute;
 	
 	void Start()
 	{
-		Screen.showCursor = false;
 		mute = false;
-	}
-	
-	void Update()
-	{
-		mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
-	}
-	
-	void OnGUI()
-	{
-		GUI.DrawTexture(new Rect(mouse.x - (w / 2), mouse.y - (h / 2), w, h), cursor);
 	}
 	
 	void OnMouseEnter()
