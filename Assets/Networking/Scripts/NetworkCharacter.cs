6338970c5b54fa03e3c47aa637468b7dc39d3766
sybiload <sybiload@gmail.com>
2015-05-02 23:03:37 +0200

    Animations are now sync over the network !

--- Assets/Networking/Scripts/NetworkCharacter.cs
+++ Assets/Networking/Scripts/NetworkCharacter.cs
@@ -5,6 +5,13 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 
 	Vector3 realPosition = Vector3.zero;
 	Quaternion realRotation = Quaternion.identity;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 	
 	void Update () 
 	{
@@ -24,13 +31,16 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 			// Sending position
 			stream.SendNext(transform.position);
 			stream.SendNext(transform.rotation);
+            stream.SendNext(anim.GetFloat("speed"));
+            stream.SendNext(anim.GetBool("jump"));
 		}
 		else
 		{
 			// Receiving position
 			realPosition = (Vector3)stream.ReceiveNext();
 			realRotation = (Quaternion)stream.ReceiveNext();
-
+            anim.SetFloat("speed", (float)stream.ReceiveNext());
+            anim.SetBool("jump", (bool)stream.ReceiveNext());
 		}
 	}
 }
