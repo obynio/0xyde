0a3628d7e9f4143a97adb90d25036aff3e3ecb68
sybiload <sybiload@gmail.com>
2015-05-03 02:43:10 +0200

    New terrain for multiplayer

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -57,7 +57,7 @@ public class MouseLook : MonoBehaviour {
 	void Start ()
 	{
 		// Make the rigid body not change rotation
-		if (GetComponent<Rigidbody>())
-			GetComponent<Rigidbody>().freezeRotation = true;
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
 	}
 }
