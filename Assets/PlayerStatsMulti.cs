56c545e3b46d3ccfcca41bfd07fd78782266bec9
Symon799 <simon.rigard@epita.fr>
2015-06-20 19:09:44 +0200

    Final Fixed multiplayer health

--- Assets/PlayerStatsMulti.cs
+++ Assets/PlayerStatsMulti.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class PlayerStatsMulti : MonoBehaviour {
 	
 	static int MaxHealth = 100;
-	public static int Health = 90;
+	public int Health = 100;
 	//var Banyan : GameObject;
 	public Texture hurtEffect;	
 	bool displayHurtEffect = false;
 	public Transform SpawnPoint;
 	public Transform Player1;
 	bool displayHurtEffect1 = false;
+	public Texture2D HpBarTexture;
+	float hpBarLength;
+	float percentOfHp;
 	
 	//var playerhit : AudioClip;
 	
@@ -38,6 +41,14 @@ public class PlayerStatsMulti : MonoBehaviour {
 	
 	void OnGUI ()
 	{
+		GUI.depth = 1;
+		
+		if (Health > 0)
+		{
+			GUI.DrawTexture(new Rect((Screen.width/2) - 100, 10, hpBarLength, 10), HpBarTexture);
+		}
+		
+
 		if(displayHurtEffect == true)
 		{
 			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), hurtEffect);
@@ -64,7 +75,12 @@ public class PlayerStatsMulti : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+		hpBarLength = Health*2f;
 		
+		if(Input.GetKeyDown("h"))
+		{
+			gameObject.BroadcastMessage("ApplyDamage", 10);		
+		}
 		if (Health >100)
 		{
 			Health = 100;
