7ba97e98aa97ba4b0084065f26f7d8e585243524
titrok <antoine.debrenne@epita.fr>
2015-06-23 15:29:13 +0200

    Added new Commands option in the menu, currently not working BUT I DONT KNOW HOW TO FUCKING DO IT! I'm still working on it, but I commit this in order to let you add the button texture.

---
+++ Assets/Commands.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Commands : MonoBehaviour {
+
+	public Texture Im;
+	private bool ison = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.renderer.enabled = JouerSimple.anglais;
+		transform.collider.enabled = JouerSimple.anglais;
+	}
+
+	void OnMouseEnter()
+	{
+		transform.localScale *= 1.1f;
+	}
+	
+	void OnMouseExit()
+	{
+		transform.localScale *= 0.909090f;
+	}
+	
+	void OnMouseUp()
+	{
+		if (!ison)
+			GUI.DrawTexture (new Rect(0,0,Im.width,Im.height), Im);
+		ison = !ison;
+	}
+
+}
