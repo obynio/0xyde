cb671c43dcaa37a5c39ea293845352de8b97e2e1
titrok <antoine.debrenne@epita.fr>
2015-06-23 16:57:12 +0200

    "Clean" Commit with music changes, have fun trying to bullshit for the remaining things...

---
+++ Assets/change_music.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class change_music : MonoBehaviour 
+{
+	public GameObject go;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player")
+		{
+			stop_musico other = (stop_musico)go.GetComponent (typeof(stop_musico));
+			other.stopmusic();
+			audio.Play();
+		}
+	}
+}
